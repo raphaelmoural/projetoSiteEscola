94b1968d0ebb55bfcb3be85ada7786758ef2368a
Sabrina-Maforte <sabrinacmaforts@gmail.com>
2023-05-17 19:46:28 -0300

    17.05

---
+++ script.js
@@ -0,0 +1,28 @@
+class MobileNavbar{
+    constructor(mobileMenu, navList){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.activeClass = 'active';
+
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick{
+        this.navList.classList.toggle(this.activeClass);
+    }
+    addClickEvent(){
+        this.mobileMenu.addEventListener('click' this.handleClick);
+    }
+
+    init(){
+        if (this.mobileMenu){
+            this.addClickEvent();
+        }
+        return this
+    }
+}
+
+const MobileNavbar = new MobileNavbar{
+    '.mobile-menu'
+    '.nav-List'
+}
