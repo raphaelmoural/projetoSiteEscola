e29a9a1db0c8fedd034d1af5bddaed197b6214d1
Sabrina-Maforte <sabrinacmaforts@gmail.com>
2023-05-31 19:29:56 -0300

    input

--- script.js
+++ script.js
@@ -56,26 +56,27 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 const images= [  
-    //image1
-      //image2
-    ];
+    './imgs/header1.jpg',
+    './imgs/header2.jpg'
+];
     
-    let currentIndex = 0;
-    const headerContainer = document.querySelector('.container');
-    const radioInputs = document.querySelectorAll('input[name="header-image"]');
-    
-    function changeHeaderImage() {
-    headerContainer.style.backgroundImage = `url('${images [currentIndex]}')`; 
-    }
-    
-    function handleRadioChange() {
-    currentIndex = parseInt(this.value, 10) - 1;
-      changeHeaderImage();
-    }
-    
-    radioInputs.forEach((input, index) => { input.addEventListener('change', handleRadioChange);  
-       input.value = index + 1;
-      });
-    
+let currentIndex = 0;
+const headerContainer = document.querySelector('header');
+const radioInputs = document.querySelectorAll('input[name="header-image"]');
+
+function changeHeaderImage() {
+headerContainer.style.backgroundImage = `url('${images[currentIndex]}')`; 
+}
+
+function handleRadioChange() {
+currentIndex = parseInt(this.value, 10) - 1;
     changeHeaderImage();
+}
+
+radioInputs.forEach((input, index) => { 
+    input.addEventListener('change', handleRadioChange);  
+    input.value = index + 1;
+});
+
+changeHeaderImage();
     
